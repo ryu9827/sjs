66e0a3d43fc445248a6ff8a4d1d3bde424fdee5a
Bruce Li <ryu9827@gmail.com>
2017-11-18 17:28:39 +1300

    Add class of boat_worker; Add get boats routing in Entity folder; Add JMSSerializerBundle; Add listAction return all boats;

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,16 +2,10 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use JMS\SerializerBundle\JMSSerializerBundle;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\boat;
-use AppBundle\Entity\worker;
-use AppBundle\Entity\boat_worker;
 use Symfony\Component\HttpFoundation\Response;
-use Doctrine\ORM\EntityManagerInterface;
 
 class DefaultController extends Controller
 {
