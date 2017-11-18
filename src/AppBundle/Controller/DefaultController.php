14fa9c2087aa1fc1bfe8a5d33a1917213b36d3e1
Bruce Li <ryu9827@gmail.com>
2017-11-18 21:04:58 +1300

    Add getBoat method and the route

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,12 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use JMS\SerializerBundle\JMSSerializerBundle;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\boat;
 use AppBundle\Entity\worker;
 use AppBundle\Entity\boat_worker;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 
 class DefaultController extends Controller
 {
@@ -19,17 +22,42 @@ class DefaultController extends Controller
         //get all boats information
         $repository = $this->getDoctrine()->getRepository(boat::class);
         $boats = $repository->findAll();
+
         $serializer = $this->container->get('jms_serializer');
         $JSON = $serializer->serialize($boats, 'json');
-//        var_dump($JSON);
+
         return new Response($JSON);
     }
 
     /**
-     * @Route("/api/boats", name="boats_list")
+     * @Route("/api/boats/{boat_id}", name="boat_show")
      */
-    public function getBoatAction($id)
+    public function getBoatAction($boat_id)
     {
+        //get one boat with workers' id
+        $em =$this->getDoctrine()->getManager();
+        $repository_boat = $em->getRepository(boat::class);
+        $boat = $repository_boat->findBy(array('id'=>$boat_id));
+
+        //if id is not found
+        if (!$boat) {
+            throw $this->createNotFoundException(
+                'No boat found for id '.$boat_id
+            );
+        }
+
+        $query = $em->createQuery(
+                     'SELECT t.worker_id from AppBundle:boat_worker t where t.boat_id = :boat_id'
+                      )->setParameter('boat_id',$boat_id);
+
+        $workers = $query->getResult();
+        foreach ($workers as $value)
+            $boat[] = $value ;
+
+        $serializer = $this->container->get('jms_serializer');
+        $JSON = $serializer->serialize($boat, 'json');
+
+        return new Response($JSON);
 
     }
 }
